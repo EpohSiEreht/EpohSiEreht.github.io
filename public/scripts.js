102b84f3e1f98de7dcb5508cf31d49a194410033
David Lee <ddlee.907@gmail.com>
2016-01-28 00:43:38 -0500

    update

--- public/scripts.js
+++ public/scripts.js
@@ -1,80 +1,18 @@
 // Behaviors
 
-function arrowHover(){
-  $('img#arrow').animate({
-    top: "90%"
+function sunHover(){
+  $('img#sun').animate({
+    top: "+5px"
   }, 1000);
-  $('img#arrow').animate({
-    top: "88%"
+  $('img#sun').animate({
+    top: "-5px"
   }, 1000, function(){
-  arrowHover();
+  sunHover();
   });
 }
 
-function arrowScroll(){
-  $('body').on('click', 'img#arrow', function(){
-    console.log('clicked');
-    $.scrollify.next();
-  })
-}
+$(function(){
+    sunHover();
+});
 
-function scrollify(){
-  $.scrollify({
-    section : ".section-class-name",
-  });
-}
 
-// $(function(){
-//     arrowHover();
-//     arrowScroll();
-//     scrollify();
-// });
-
-// $('h2.logo').css('position', 'absolute');
-
-// function moveLogo(){
-//   $('h2.logo').animate({
-//     top: "20px",
-//     left: "95px"
-//   }, 500);
-//   $('h2.logo').animate({
-//     top: "80px",
-//     left: "70px"
-//   }, 500);
-//   $('h2.logo').animate({
-//     top: "20px",
-//     left: "45px"
-//   }, 500);
-//   $('h2.logo').animate({
-//     top: "80px",
-//     left: "70px"
-//   }, 500);
-// }
-
-// setInterval(moveLogo, 2000);
-
-// function moveElement(){
-//   // Move element one direction
-//   $('element').animate({
-//     css_property: "value",
-//     css_property: "value"
-//   }, duration);
-//   // Move element back to original position
-//   $('element').animate({
-//     css_property: "value",
-//     css_property: "value"
-//   }, duration);
-//   // Move element to another direction
-//   $('element').animate({
-//     css_property: "value",
-//     css_property: "value"
-//   }, duration);
-//   // Move element back to original position
-//   $('element').animate({
-//     css_property: "value",
-//     css_property: "value"
-//   }, duration, function(){
-//     // This function allows you to add a new task after all animations have taken place.
-//   });
-
-// }
