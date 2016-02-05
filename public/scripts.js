8e1377c6e193328994bfe0e0f6babffccfdca8d9
David Lee <ddlee.907@gmail.com>
2016-02-05 11:31:53 -0500

    update

--- public/scripts.js
+++ public/scripts.js
@@ -1,66 +1,89 @@
 // Behaviors
 
-// function expandAbout(){
-//   $('body').on('click', 'img#about', function(){
-//     $('.icon').css('opacity', '0');
-//     $('.expand-icon').css('opacity', '0');
-//     $('img#about').css('opacity', '1');
-//     $('#pink-circle').css('opacity', '1');
-//     $('.expand-icon').animate({
-//       width: "100%",
-//       height: "100%",
-//       left: "-50%",
-//       top: "-50%",
-//     }, 1000, function(){
-//       $('.expand-icon').css({'width': '100%','height': '100%','left': '0','top': '0', 'border-radius': '0'});
-//     });
-//   });
-// };
-
 function closeX(){
   $('body').on('click', '.x-icon', function(){
     $(this).parent().hide();
   });
 }
 
-function openAbout(){
+function openIcons(){
   $('body').on('click', 'img#about', function(){
     $('.about-section').show();
   });
-}
-
-function openSkills(){
   $('body').on('click', 'img#photo', function(){
     $('.photo-section').show();
   });
-}
-
-function openPortfolio(){
   $('body').on('click', 'img#portfolio', function(){
     $('.portfolio-section').show();
   });
-}
-
-function openContact(){
   $('body').on('click', 'img#contact', function(){
     $('.contact-section').show();
   });
 }
 
+function animateAboutIcon(){
+  $('img#about').animate({
+    top: "+=10px"
+  }, 800, function(){
+    $('img#about').animate({
+      top: "-=10px"
+    }, 800, function(){
+      animateAboutIcon();
+    })
+  });
+}
 
+function animateSkillsIcon(){
+    $('img#photo').animate({
+      top: "+=10px"
+    }, 800, function(){
+      $('img#photo').animate({
+        top: "-=10px"
+      }, 800, function(){
+        animateSkillsIcon();
+      })
+    });
+}
 
-/*div.expand-icon{
-  width: 100%;
-  height: 100%;
-  top: 60px;
-}*/
+function animatePortfolioIcon(){
+  $('img#portfolio').animate({
+    top: "+=10px"
+  }, 800, function(){
+    $('img#portfolio').animate({
+      top: "-=10px"
+    }, 800, function(){
+      animatePortfolioIcon();
+    })
+  });
+}
+
+function animateContactIcon(){
+  $('img#contact').animate({
+    top: "+=10px"
+  }, 800, function(){
+    $('img#contact').animate({
+      top: "-=10px"
+    }, 800, function(){
+      animateContactIcon();
+    })
+  });
+}
 
 $(function(){
 
-  openAbout();
-  openSkills();
-  openPortfolio();
-  openContact();
+  animateAboutIcon();
+  setTimeout(function(){
+    animateSkillsIcon();
+  }, 400);
+  setTimeout(function(){
+    animatePortfolioIcon();
+  }, 800);
+  setTimeout(function(){
+    animateContactIcon();
+  }, 1200);
+
+  openIcons();
+
   closeX();
 
 });
