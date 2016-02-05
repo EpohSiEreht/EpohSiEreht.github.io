96ab7fd48661121efb83e38fe593b12bc30a181d
David Lee <ddlee.907@gmail.com>
2016-02-04 20:10:39 -0500

    update

--- public/scripts.js
+++ public/scripts.js
@@ -1,18 +1,30 @@
 // Behaviors
 
-function sunHover(){
-  $('img#sun').animate({
-    top: "+10px"
-  }, 1000);
-  $('img#sun').animate({
-    top: "-10px"
-  }, 1000, function(){
-  sunHover();
-  });
-}
+// function expandAbout(){
+//   $('body').on('click', 'img#about', function(){
+//     $('.icon').css('opacity', '0');
+//     $('.expand-icon').css('opacity', '0');
+//     $('img#about').css('opacity', '1');
+//     $('#pink-circle').css('opacity', '1');
+//     $('.expand-icon').animate({
+//       width: "100%",
+//       height: "100%",
+//       left: "-50%",
+//       top: "-50%",
+//     }, 1000, function(){
+//       $('.expand-icon').css({'width': '100%','height': '100%','left': '0','top': '0', 'border-radius': '0'});
+//     });
+//   });
+// };
+
+/*div.expand-icon{
+  width: 100%;
+  height: 100%;
+  top: 60px;
+}*/
 
 $(function(){
-    sunHover();
+
+
+
 });
-
-
