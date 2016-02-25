f632870ec19d706ffa4787230bb5b27065be17ab
David Lee <ddlee.907@gmail.com>
2016-02-25 18:24:28 -0500

    redesign entire page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,138 @@
-$(function(){
-	$('body').on('click', '#menu', function(){
-		$('.mobile-nav').show();
-		$('.mobile-nav').animate({
-			transform: "translateY(100%)"
-		}, 300);
+$(function() {
+	toggleHamburger();
+	navLinkTransition();
+	contentTransition();
+	projectHover();
+});
+
+var firstContentRight = $('#first-content').css('right');
+var secondContentRight = $('#second-content').css('right');
+var thirdContentRight = $('#third-content').css('right');
+
+function contentTransition() {
+	$('body').on('click', '#to-projects', function() {
+		rightProjects();
 	});
-})
+	$('body').on('click', '#to-about', function() {
+		leftAbout();
+	});
+	$('body').on('click', '#to-contact', function() {
+		rightContact();
+	});
+	$('body').on('click', '#back-projects', function() {
+		leftProjects();
+	});
+}
+
+function rightProjects(){
+	$('#first-content').animate({
+		right: '100vw'
+	}, 500);
+	$('#second-content').css('right', '-100vw').show().animate({
+		right: '0'
+	}, 500);
+}
+
+function rightContact(){
+	$('#second-content').animate({
+		right: '100vw'
+	}, 500);
+	$('#third-content').css('right', '-100vw').show().animate({
+		right: '0'
+	}, 500);
+}
+
+function leftAbout(){
+	$('#second-content').animate({
+		right: '-100vw'
+	}, 500);
+	$('#first-content').show().animate({
+		right: '0'
+	}, 500);
+}
+
+function leftProjects(){
+	$('#second-content').animate({
+		right: '0vw'
+	}, 500);
+	$('#third-content').animate({
+		right: '-100vw'
+	}, 500);
+}
+
+function navLinkTransition() {
+	$('#nav-about').on('click', function(e){
+		e.preventDefault();
+		$('#nav-icon').trigger('click');
+		if((firstContentRight === "0px" && secondContentRight !== "0px") || (firstContentRight !== "0px" && secondContentRight === "0px")){
+			leftAbout();
+		} else if(firstContentRight !== "0px"){
+			leftProjects();
+			setTimeout(function(){
+				leftAbout();
+			}, 500);
+		}
+	});
+	$('#nav-projects').on('click', function(e){
+		e.preventDefault();
+		$('#nav-icon').trigger('click');
+		if((firstContentRight === "auto" && secondContentRight === "auto") || (firstContentRight !== "0px" && secondContentRight !== "0px")){
+			rightProjects();
+		} else if(firstContentRight > "0px" && thirdContentRight !== "0px"){
+			leftProjects();
+		}
+	});
+	$('#nav-contact').on('click', function(e){
+		e.preventDefault();
+		$('#nav-icon').trigger('click');
+		if((firstContentRight == "0px" && secondContentRight !== "0px" & thirdContentRight !== "0px") || firstContentRight !== "0px" && secondContentRight !== "0px" && thirdContentRight === "0px"){
+			rightContact();
+		} else if((firstContentRight !== "0px" && secondContentRight !== "0px" && thirdContentRight !== "0px") || (firstContentRight !== "0px" && thirdContentRight !== "0px" && secondContentRight === "0px")){
+			rightProjects();
+			setTimeout(function(){
+				rightContact();
+			}, 500);
+		}
+	});
+}
+
+function projectHover() {
+	$('.project-tile').hover(function(){
+		var thisHover = $(this).find('.hover-tile');
+		var thisH = $(this).find('h3');
+		$(thisHover).show();
+		$(thisHover).animate({
+			height: '100%'
+		}, 700)},
+		function(){
+			var thisHover = $(this).find('.hover-tile');
+			var thisH = $(this).find('h3');
+			$(thisHover).animate({
+				height: '0'
+			}, 700);
+			setTimeout(function(){
+				$(thisHover).hide();
+			},700);
+		}
+	);
+}
+
+function toggleHamburger() {
+    $('#nav-icon').click(function(){
+        $(this).toggleClass('open');
+        var dropHeight = $('#menu-drop').get(0).scrollHeight + "px";
+        if($('#menu-drop').css('height') === "0px"){
+        	$('#menu-drop').show().animate({
+        		height: $('#menu-drop').get(0).scrollHeight
+        	}, 500);
+        } else if($('#menu-drop').css('height') === dropHeight){
+        	$('#menu-drop').animate({
+        		height: '0px'
+        	}, 500);
+    		setTimeout(function(){
+    			$('#menu-drop').hide();
+    		}, 500);
+        }
+    });
+
+}
