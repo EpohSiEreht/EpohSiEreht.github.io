6787d55645f0124986961713462795e48658a785
David Lee <ddlee.907@gmail.com>
2016-06-06 14:16:10 -0400

    Prepare site for Gulp and SASS makeover

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    useref      = require('gulp-useref'),
+    gulpIf      = require('gulp-if'),
+    uglify      = require('gulp-uglify'),
+    cssnano     = require('gulp-cssnano'),
+    imagemin    = require('gulp-imagemin'),
+    cache       = require('gulp-cache'),
+    del         = require('del'),
+    runSeq      = require('run-sequence');
+
+// ------------------
+// Developement Tasks
+// ------------------
+
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  })
+});
+
+gulp.task('sass', function () {
+  return gulp.src('assets/scss/**/*.scss')
+             .pipe(sass())
+             .pipe(gulp.dest('assets/css'))
+             .pipe(browserSync.reload({
+               stream: true
+             }));
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+  gulp.watch('**/*.scss', ['sass']);
+  gulp.watch('/*.html', browserSync.reload);
+  gulp.watch('**/*.js', browserSync.reload);
+});
+
+// ------------------
+// Optimization Tasks
+// ------------------
+
+gulp.task('useref', function () {
+  return gulp.src('*.html')
+             .pipe(useref())
+             .pipe(gulpIf('*.js', uglify()))
+             .pipe(gulpIf('*.css', cssnano()))
+             .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('assets/img/**/*.+(png|jpg|gif|svg)')
+             .pipe(cache(imagemin({
+               interlaced: true
+             })))
+             .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('vendor/**/*')
+             .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('clean:dist', function () {
+  return del.sync('dist');
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', ['sass', 'useref', 'images', 'fonts'], callback);
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['sass', 'browserSync', 'watch'], callback);
+});
